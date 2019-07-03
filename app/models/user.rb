6ed81685342f8c4fca5920f7ac2e23683f8754ee
Luis Mario Urrea-Murillo <sinourain@gmail.com>
2019-07-03 07:45:41 -0500

    Set default role when a new user is created. Set ruby version

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  ROLES = %i[admin client]
+
+  after_initialize :set_default_role, if: :new_record?
+
+  private
+
+  def set_default_role
+    self.role ||= :client
+  end
 end
